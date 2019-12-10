c0461730f8731275572b30e5d677f72cadf9402e
Shiaw-Rong Lin <lin_rong2005@hotmail.com>
2019-12-10 08:13:37 -0500

    art level update

---
+++ PlatformerPrototype/Assets/Scripts/ButtonTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTrigger : MonoBehaviour
+{
+
+    public GameObject movePlatform1;
+    public GameObject movePlatform2;
+
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject.Find(movePlatform1.name).GetComponent<MovePlatform>().enabled = true;
+        GameObject.Find(movePlatform2.name).GetComponent<MovePlatform>().enabled = true;
+
+    }
+
+}
