5ee4db01056f724f48423bdda2c251da0fa9799c
Stephen Brasel <wearyroad@gmail.com>
2019-12-10 10:08:25 -0500

    Spikes hurt, hurt image tells the playe rthey'ev been hurt but doesn't hurt them yet.

---
+++ PlatformerPrototype/Assets/Scripts/FadeImage.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeImage : MonoBehaviour
+{
+	[Range(0, 1)] [SerializeField] private float startAlpha = 0.673f;
+	[SerializeField] private Image image;
+	[SerializeField] private float fadeSpeed = 0.9f;
+
+    void Awake()
+    {
+		if(image == null)
+		{
+			image = GetComponent<Image>();
+		}
+
+		Color tempColor = image.color;
+		tempColor.a = 0f;
+		image.color = tempColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Color tempColor = image.color;
+		if(tempColor.a > 0f)
+		{
+			float tempA = tempColor.a - (fadeSpeed * Time.unscaledDeltaTime);
+			tempColor.a = Mathf.Clamp(tempA, 0f, 1f);
+			image.color = tempColor;
+		}
+	}
+
+	public void ResetAlpha()
+	{
+		Color tempColor = image.color;
+		tempColor.a = startAlpha;
+		image.color = tempColor;
+	}
+}
---
+++ PlatformerPrototype/Assets/Scripts/Spikes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+	GameObject hurtImage;
+
+	private void Awake()
+	{
+		hurtImage = GameObject.Find("HurtImage");
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			Damage();
+		}
+	}
+
+	private void Damage()
+	{
+		if (hurtImage == null) return;
+		FadeImage fadeImage = hurtImage.GetComponent<FadeImage>();
+		if (fadeImage == null) return; 
+		fadeImage.ResetAlpha();
+	}
+}
