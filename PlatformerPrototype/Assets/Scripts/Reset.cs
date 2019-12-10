98bdf4e73a66f35ffe3ec88d6b67c4c2a8bbdcac
Stephen Brasel <wearyroad@gmail.com>
2019-12-10 11:19:48 -0500

    Reset resets the scene.

--- PlatformerPrototype/Assets/Scripts/Reset.cs
+++ PlatformerPrototype/Assets/Scripts/Reset.cs
@@ -4,27 +4,39 @@ using UnityEngine;
 
 public class Reset : MonoBehaviour
 {
-
 	private Vector3 startPosition;
-    // Start is called before the first frame update
-    void Awake()
+	private Quaternion startRotation;
+	private Vector3 startScale;
+	private string levelName = "";
+	// Start is called before the first frame update
+	void Awake()
     {
 		startPosition = transform.position;
-    }
+		startRotation = transform.rotation;
+		startScale = transform.localScale;
+		levelName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+	}
 
 	public void ResetEverything()
 	{
-		transform.position = startPosition;
-		Rigidbody rb = GetComponent<Rigidbody>();
-		if (rb)
-		{
-			rb.velocity = Vector3.zero;
-		}
-		CharacterController cc = GetComponent<CharacterController>();
-		if (cc)
-		{
-			cc.center = startPosition;
-			cc.SimpleMove(Vector3.zero);
-		}
+		UnityEngine.SceneManagement.SceneManager.LoadScene(levelName);
+		//transform.position = startPosition;
+		//transform.rotation= startRotation;
+		//transform.localScale = startScale;
+		//CharacterController cc = GetComponent<CharacterController>();
+		//if (cc)
+		//{
+		//	cc.velocity.Set(0, 0, 0);
+		//	//cc.transform.position = startPosition;
+		//	//cc.transform.rotation = startRotation;
+		//	//cc.transform.localScale = startScale;
+		//	return;
+		//}
+		//Rigidbody rb = GetComponent<Rigidbody>();
+		//if (rb)
+		//{
+		//	rb.velocity = Vector3.zero;
+		//	return;
+		//}
 	}
 }
