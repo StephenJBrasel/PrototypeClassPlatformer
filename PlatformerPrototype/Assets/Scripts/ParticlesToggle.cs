8f7120dd9084591b7856320d4022e82b88367c71
Stephen Brasel <wearyroad@gmail.com>
2019-12-16 18:57:30 -0500

    Art Scene demos the particle effects turning on and off.

---
+++ PlatformerPrototype/Assets/Scripts/ParticlesToggle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlesToggle : MonoBehaviour
+{
+    [SerializeField] private List<ParticleSystem> particlesToToggle;
+    private PlayerControllerCC playerControllerCC;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerControllerCC = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControllerCC>();
+        playerControllerCC.OnTimeSlowEvent.AddListener(ToggleParticles);
+        playerControllerCC.OnTimeResumeEvent.AddListener(ToggleParticles);
+    }
+
+    public void ToggleParticles()
+    {
+        for (int i = 0; i < particlesToToggle.Count; i++)
+        {
+            particlesToToggle[i].gameObject.SetActive(!particlesToToggle[i].gameObject.activeSelf);
+            //var emission = particlesToToggle[i].emission;
+            //emission.enabled = !particlesToToggle[i].emission.enabled;
+        }
+    }
+}
---
+++ PlatformerPrototype/Assets/Scripts/UnscaleTimeForParticles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UnscaleTimeForParticles : MonoBehaviour
+{
+    private ParticleSystem pSystem;
+    private ParticleSystem.MainModule pMain;
+
+    private void Awake()
+    {
+        pSystem = GetComponent<ParticleSystem>();
+        pMain = pSystem.main;
+    }
+
+    private void Update()
+    {
+        //pMain.useUnscaledTime = true;
+        pSystem.Simulate(Time.unscaledDeltaTime, true, false);
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    pSystem.Simulate(Time.unscaledDeltaTime, true, false);
+    //}
+}
