8f1d229789edb35a50ffe0cdca1c2843879735dd
Shiaw-Rong Lin <lin_rong2005@hotmail.com>
2019-12-22 17:40:26 -0500

    Art3 added

---
+++ PlatformerPrototype/Assets/Scripts/PuzzleText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzleText : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject.Find("Puzzle_Text").GetComponent<Text>().text = transform.GetComponent<ButtonTrigger>().objectsToTrigger[0].name + " unlocked !";
+        GameObject.Find("Puzzle_Text").GetComponent<Text>().enabled = true;
+        StartCoroutine(unlockText());
+    }
+
+    IEnumerator unlockText()
+    {
+        yield return new WaitForSeconds(3);
+        GameObject.Find("Puzzle_Text").GetComponent<Text>().enabled = false;
+    }
+
+}
